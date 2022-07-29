740f571e39961999ce0ee0d9daa43981ec9548fc
BasketballForce <28318597+Basketballforce@users.noreply.github.com>
2022-07-28 23:01:43 -0400

    move outside of html and update progress bar logic to its own file

--- progress.js
+++ progress.js
@@ -1,50 +1,31 @@
-// grab progress bar
-const pageProgressBar = document.getElementById("progress-bar")
-
-// grab and return scroll container. height that scrollbar should be bound to
-const scrollContainer = () => {
-  return document.getElementById("progress-container")
-}
-
-// set the scrollbar width when loaded in
-document.addEventListener("DOMContentLoaded", () =>{
-    progressWidth()
-
-    // change scrollbar width when scrolling. using throttle to limit function calls
-    document.addEventListener('scroll', throttle( progressWidth, 15 ) )
-
-    // change scrollbar width when resizing
-    window.addEventListener('resize', function(event) {
-        progressWidth()
-    })
-
-})
-
-// function to change scrollbar width accordingly
-const progressWidth= () => {
-    //console.log("Scroll Height: ", scrollContainer().scrollHeight)
-    //console.log("Client Height: ", scrollContainer().clientHeight)
-    //console.log("function throttle check")
-
-    // calcualte scroll percentage relative to the container
-    const scrolledPercentage =
-      (scrollContainer().scrollTop /
-        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-      100
-  
-    // plus 1 enables a range of 1% to 101%. Stylistic choice
-    pageProgressBar.style.width = `${scrolledPercentage+ 1}%`
-}
-
-
-// throttle scroll limiter
-// wait is in milliseconds
-function throttle( fn, wait ){
+  // throttle scroll limiter
+  // wait is in milliseconds
+  function throttle(fn, wait) {
     let lastCall = 0;
-      return function(){
-      if( Date.now() - lastCall > wait  ){
+    return function () {
+      if (Date.now() - lastCall > wait) {
         lastCall = Date.now()
         fn()
       }
     }
+  }
+
+  // When the user scrolls the page, change progress bar width
+  document.addEventListener('scroll', throttle(progressWidth, 15))
+  // change scrollbar width when resizing
+  window.addEventListener('resize', function (event) {
+    progressWidth()
+  })
+
+  document.addEventListener("DOMContentLoaded", () => {
+    progressWidth()
+  })
+
+  function progressWidth() {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrolled = Math.round(scrollPercent * 100);
+    document.getElementById("progressBar").style.width = scrolled + 3 + "%";
   }
